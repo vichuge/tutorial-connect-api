30e030445f94d4c4fc20984ce9ac01c094211f87
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-06-28 13:07:13 -0500

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import TodoApp from './TodoApp';
 
+// we add this libraries to connect
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<TodoApp />, rootElement);
+ReactDOM.render(
+  // we add <Provider with the respective store
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement);
