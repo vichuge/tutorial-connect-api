771f1432ed6ab97a917dc246c6c147f699fdefed
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-06-29 15:58:46 -0500

    Restart project, without redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import TodoApp from "./TodoApp";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
-
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
-  rootElement
-);
+ReactDOM.render(<TodoApp />, rootElement);
