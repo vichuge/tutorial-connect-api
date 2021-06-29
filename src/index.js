622da27a196f0b32abcb32ab3c28e060c5200ace
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-06-29 15:13:21 -0500

    Provide the store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import TodoApp from "./TodoApp";
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<TodoApp />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
+);
